a4ce84603e67904e21b5a60a9d4655e80b09f76a
DenysSkachko <denya.skachko@gmail.com>
2025-09-12 19:56:17 +0300

    Make template App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,34 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import { About } from './sections/About'
+import Explore from './sections/Explore'
+import GetStarted from './sections/GetStarted'
+import WhatsNew from './sections/WhatsNew'
+import World from './sections/World'
+import Insights from './sections/Insights'
+import Feedback from './sections/Feedback'
 
 function App() {
-
   return (
     <main className="bg-primary overflow-hidden">
-    <Navbar />
-    <Hero />
-    <div className="relative">
-      <About />
-      <div className="gradient-03 z-0"/>
-    </div>
+      <Navbar />
+      <Hero />
+      <div className="relative">
+        <About />
+        <div className="gradient-03 z-0" />
+        <Explore />
+      </div>
+      <div className="relative">
+        <GetStarted />
+        <div className="gradient-04 z-0" />
+        <WhatsNew />
+      </div>
+      <World />
+      <div className="relative">
+        <Insights />
+        <div className="gradient-04 z-0" />
+        <Feedback />
+      </div>
     </main>
   )
 }
